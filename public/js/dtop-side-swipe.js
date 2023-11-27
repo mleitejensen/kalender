f564eda4bba629f42e3f00d3ef6c3db5957736a0
Nyan <nyandude22@gmail.com>
2023-11-27 10:26:31 +0100

    Fixed arrow bug

--- public/js/dtop-side-swipe.js
+++ public/js/dtop-side-swipe.js
@@ -1,70 +1,75 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('DOM content loaded');
-   var currentDayInDecember = new Date().getDate();
+    var currentDayInDecember = new Date().getDate();
 
-   document.addEventListener('keyup', function(e) {
-   console.log("Key released:", e.key);
+    document.addEventListener('keyup', function(e) {
+        console.log("Key released:", e.key);
 
-   if (e.key === 'Escape') {
-       window.location.replace('/');
-   }
-   });
+        // Check if the focused element is an input field
+        if (document.activeElement.tagName.toLowerCase() === 'input') {
+            return;
+        }
 
+        if (e.key === 'Escape') {
+            window.location.replace('/');
+        }
+    });
 
-   document.addEventListener('keydown', function(e) {
-       console.log("Key pressed:", e.key);
+    document.addEventListener('keydown', function(e) {
+        // Check if the focused element is an input field
+        if (document.activeElement.tagName.toLowerCase() === 'input') {
+            return;
+        }
 
-       var direction = e.key === 'ArrowLeft' ? 'left' : e.key === 'ArrowRight' ? 'right' : '';
+        var direction = e.key === 'ArrowLeft' ? 'left' : e.key === 'ArrowRight' ? 'right' : '';
 
-       if (direction !== '') {
-           var pathParts = window.location.pathname.split('/');
-           var itemNumberIndex = pathParts.indexOf('december') + 1;
-           var itemNumber = itemNumberIndex < pathParts.length ? parseInt(pathParts[itemNumberIndex]) : NaN;
+        if (direction !== '') {
+            var pathParts = window.location.pathname.split('/');
+            var itemNumberIndex = pathParts.indexOf('december') + 1;
+            var itemNumber = itemNumberIndex < pathParts.length ? parseInt(pathParts[itemNumberIndex]) : NaN;
 
-           console.log("Item Number:", itemNumber);
+            console.log("Item Number:", itemNumber);
 
-           if (!isNaN(itemNumber)) {
-               var nextItemNumber;
+            if (!isNaN(itemNumber)) {
+                var nextItemNumber;
 
+                if (direction === 'left' && itemNumber === 1) {
+                    // Left arrow at item 1, redirect to root
+                    var rootUrl = '/';
+                    console.log("Redirecting to root:", rootUrl);
+                    window.location.assign(rootUrl);
+                    return;
+                }
 
-               if (direction === 'left' && itemNumber === 1) {
-                   // Left arrow at item 1, redirect to root
-                   var rootUrl = '/';
-                   console.log("Redirecting to root:", rootUrl);
-                   window.location.assign(rootUrl);
-                   return;
-               }
+                if (direction === 'left' && itemNumber > 1) {
+                    // Left arrow (go to the previous date)
+                    nextItemNumber = itemNumber - 1;
+                } else if (direction === 'right') {
+                    // Right arrow (go to the next date)
+                    nextItemNumber = itemNumber + 1;
+                }
 
-               if (direction === 'left' && itemNumber > 1) {
-                   // Left arrow (go to the previous date)
-                   nextItemNumber = itemNumber - 1;
-               } else if (direction === 'right') {
-                   // Right arrow (go to the next date)
-                   nextItemNumber = itemNumber + 1;
-               }
+                console.log("Next Item Number:", nextItemNumber);
 
-               console.log("Next Item Number:", nextItemNumber);
+                if (nextItemNumber <= currentDayInDecember) {
+                    var nextItemUrl = '/' + nextItemNumber + '/';
+                    console.log('Next item URL:', nextItemUrl);
 
-               if (nextItemNumber <= currentDayInDecember) {
-                   var nextItemUrl = '/' + nextItemNumber + '/';
-                   console.log('Next item URL:', nextItemUrl);
+                    // Update the URL without 'december' part
+                    pathParts[itemNumberIndex] = nextItemNumber;
 
-                   // Update the URL without 'december' part
-                   pathParts[itemNumberIndex] = nextItemNumber;
+                    var finalUrl = pathParts.join('/');
+                    console.log('Final URL:', finalUrl);
 
-                   var finalUrl = pathParts.join('/');
-                   console.log('Final URL:', finalUrl);
+                    localStorage.setItem('box' + itemNumber, 'opened');
+                    localStorage.setItem('box' + nextItemNumber, 'opened');
 
-                   localStorage.setItem('box' + itemNumber, 'opened');
-                   localStorage.setItem('box' + nextItemNumber, 'opened');
-
-                   // Directly update the pathname
-                   window.location.pathname = finalUrl;
-               } else {
-                   alert("Ikke den datoen ennå");
-               }
-           }
-       }
-   });
-
+                    // Directly update the pathname
+                    window.location.pathname = finalUrl;
+                } else {
+                    alert("Ikke den datoen ennå");
+                }
+            }
+        }
+    });
 });
